e4551b01fbd5c47ec37e05f810cb7fb8e3a7b88c
joeljawak <joeljawak20@gmail.com>
2026-05-16 12:53:47 +0700

    tabel

--- database/migrations/2026_05_14_132802_create_dokter_table.php
+++ database/migrations/2026_05_14_132802_create_dokter_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +13,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('dokter', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+
+            $table->id('ID_Dokter');
+
+            $table->unsignedBigInteger('ID_User');
+
+            $table->string('Spesialis');
+
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->timestamp('updated_at')
+                ->nullable()
+                ->useCurrentOnUpdate();
+
+            $table->foreign('ID_User')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
--- database/migrations/2026_05_14_132854_create_pasien_table.php
+++ database/migrations/2026_05_14_132854_create_pasien_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +13,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pasien', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+
+            $table->id('ID_Pasien');
+            $table->unsignedBigInteger('ID_User');
+            $table->string('No_BPJS')->nullable();
+            $table->text('Riwayat_Penyakit')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')
+                ->nullable()
+                ->useCurrentOnUpdate();
+            $table->foreign('ID_User')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
